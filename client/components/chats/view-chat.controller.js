21b4c5df67f8365057a61e3100303cfde8fe6211
Brian <brianflaherty64@gmail.com>
2016-03-01 15:23:01 -0500

    added input form for creating messages

--- client/components/chats/view-chat.controller.js
+++ client/components/chats/view-chat.controller.js
@@ -2,10 +2,16 @@ angular
   .module('Whatsnoodle')
   .controller('ViewChatCtrl', ViewChatCtrl);
  
-function ViewChatCtrl ($scope, $reactive, $stateParams) {
+function ViewChatCtrl ($scope, $reactive, $stateParams, $timeout, $ionicScrollDelegate) {
   $reactive(this).attach($scope);
  
   let chatId = $stateParams.chatId;
+  let isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+
+  this.sendMessage = sendMessage;
+  this.inputUp = inputUp;
+  this.inputDown = inputDown;
+  this.closeKeyboard = closeKeyboard;
  
   this.helpers({
     messages() {
@@ -15,4 +21,30 @@ function ViewChatCtrl ($scope, $reactive, $stateParams) {
       return Chats.findOne(chatId);
     }
   });
+
+  function sendMessage () {
+
+  }
+
+  function inputUp () {
+    if (isIOS) {
+      this.keyboardHeight = 216;
+    }
+ 
+    $timeout(function() {
+      $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(true);
+    }, 300);
+  }
+ 
+  function inputDown () {
+    if (isIOS) {
+      this.keyboardHeight = 0;
+    }
+ 
+    $ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+ 
+  function closeKeyboard () {
+    // cordova.plugins.Keyboard.close();
+  }
 }
